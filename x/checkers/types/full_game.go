c8870d7715234c27f91b3fe7b4fc49a25a5729ff
Ihor Sikachyna <ihor.sikachyna@gmail.com>
2024-12-09 13:18:41 -0500

    Fixed errors package, added game type tests

--- x/checkers/types/full_game.go
+++ x/checkers/types/full_game.go
@@ -3,29 +3,29 @@ package types
 import (
 	"fmt"
 
+	errorsmod "cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/igor-sikachyna/checkers/x/checkers/rules"
 )
 
 func (storedGame StoredGame) GetBlackAddress() (black sdk.AccAddress, err error) {
 	black, errBlack := sdk.AccAddressFromBech32(storedGame.Black)
-	return black, sdkerrors.Wrapf(errBlack, ErrInvalidBlack.Error(), storedGame.Black)
+	return black, errorsmod.Wrapf(errBlack, ErrInvalidBlack.Error(), storedGame.Black)
 }
 
 func (storedGame StoredGame) GetRedAddress() (red sdk.AccAddress, err error) {
 	red, errRed := sdk.AccAddressFromBech32(storedGame.Red)
-	return red, sdkerrors.Wrapf(errRed, ErrInvalidRed.Error(), storedGame.Red)
+	return red, errorsmod.Wrapf(errRed, ErrInvalidRed.Error(), storedGame.Red)
 }
 
 func (storedGame StoredGame) ParseGame() (game *rules.Game, err error) {
 	board, errBoard := rules.Parse(storedGame.Board)
 	if errBoard != nil {
-		return nil, sdkerrors.Wrapf(errBoard, ErrGameNotParseable.Error())
+		return nil, errorsmod.Wrapf(errBoard, ErrGameNotParseable.Error())
 	}
 	board.Turn = rules.StringPieces[storedGame.Turn].Player
 	if board.Turn.Color == "" {
-		return nil, sdkerrors.Wrapf(fmt.Errorf("turn: %s", storedGame.Turn), ErrGameNotParseable.Error())
+		return nil, errorsmod.Wrapf(fmt.Errorf("turn: %s", storedGame.Turn), ErrGameNotParseable.Error())
 	}
 	return board, nil
 }
