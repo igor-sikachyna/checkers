ac07870d3e4384e68070fd9c350cb9a009cf7ac4
Ihor Sikachyna <ihor.sikachyna@gmail.com>
2024-12-09 12:58:22 -0500

    Added default game ID initialization

--- x/checkers/types/genesis_test.go
+++ x/checkers/types/genesis_test.go
@@ -22,7 +22,7 @@ func TestGenesisState_Validate(t *testing.T) {
 			desc: "valid genesis state",
 			genState: &types.GenesisState{
 
-				SystemInfo: &types.SystemInfo{
+				SystemInfo: types.SystemInfo{
 					NextId: 38,
 				},
 				StoredGameList: []types.StoredGame{
@@ -63,4 +63,13 @@ func TestGenesisState_Validate(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestDefaultGenesisState_ExpectedInitialNextId(t *testing.T) {
+	require.EqualValues(t,
+		&types.GenesisState{
+			StoredGameList: []types.StoredGame{},
+			SystemInfo:     types.SystemInfo{uint64(1)},
+		},
+		types.DefaultGenesis())
 }
