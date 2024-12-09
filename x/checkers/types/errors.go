ebf4301d64e102ac058271c355606bad8f3de017
Ihor Sikachyna <ihor.sikachyna@gmail.com>
2024-12-09 13:03:36 -0500

    Added state and argument validation

--- x/checkers/types/errors.go
+++ x/checkers/types/errors.go
@@ -8,6 +8,8 @@ import (
 
 // x/checkers module sentinel errors
 var (
-	ErrInvalidSigner = sdkerrors.Register(ModuleName, 1100, "expected gov account as only signer for proposal message")
-	ErrSample        = sdkerrors.Register(ModuleName, 1101, "sample error")
+	ErrInvalidSigner    = sdkerrors.Register(ModuleName, 1100, "expected gov account as only signer for proposal message")
+	ErrInvalidBlack     = sdkerrors.Register(ModuleName, 1101, "black address is invalid: %s")
+	ErrInvalidRed       = sdkerrors.Register(ModuleName, 1102, "red address is invalid: %s")
+	ErrGameNotParseable = sdkerrors.Register(ModuleName, 1103, "game cannot be parsed")
 )
